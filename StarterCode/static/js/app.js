8290d4fd2497212a9aa4dad6af4d7ef5521637b5
mkrenicki <73031837+mkrenicki@users.noreply.github.com>
2021-02-21 17:24:33 -0500

    second commit - working on js app

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,9 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get reference to table body
+var tbody = d3.select("tbody");
+
+// Test of console log of the data in data.js
+// console.log(data);
+
